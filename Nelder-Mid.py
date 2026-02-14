45ad8936886237e916b7d9e817aef02d78761bd8
Савинский Владимир <vovka_savinskiy@mail.ru>
2026-02-14 09:57:55 +0300

    Добавлен вывод информации о решении по итерациям

--- Nelder-Mid.py
+++ Nelder-Mid.py
@@ -19,23 +19,30 @@ class Optimizer:
         # расчёт двух вершин начального симплекса
         delta1 = (sqrt(self.n + 1) - 1) / (self.n * sqrt(2)) * self.m
         delta2 = (sqrt(self.n + 1) + self.n - 1) / (self.n * sqrt(2)) * self.m
+        print(f"Приращения\ndelta1 = {round(delta1, 3)}\ndelta2 = {round(delta2, 3)}")
         self.x[1] = [self.x[0][0] + delta1, self.x[0][1] + delta2]
         self.x[2] = [self.x[0][0] + delta2, self.x[0][1] + delta1]
+        print(f"Исходный базис\n{self.x[0][0]} {self.x[0][1]}\n"
+              f"{round(self.x[1][0], 3)} {round(self.x[1][1], 3)}\n"
+              f"{round(self.x[2][0], 3)} {round(self.x[2][1], 3)}\n")
         self.F = [self.evaluate(self.x[i]) for i in range(self.n + 1)] # оценка вершин
 
         iteration = 0
         while True:
+            print(f"Итерация: {iteration}")
             k1, k2, k = np.argsort(self.F) # сортировка вершин
             # определение центра тяжести (без максимальной вершины)
             xc = [
                 (self.x[k1][0] + self.x[k2][0]) / self.n,
                 (self.x[k1][1] + self.x[k2][1]) / self.n
             ]
+            print(f"Центр тяжести: {round(xc[0], 3)} {round(xc[1], 3)}")
             # отражение максимальной вершины
             x = [
                 2 * xc[0] - self.x[k][0],
                 2 * xc[1] - self.x[k][1]
             ]
+            print(f"Отражённая вершина: {round(x[0], 3)} {round(x[1], 3)}")
             F = self.evaluate(x)
 
             squeeze_flag, reduction_flag = False, False
@@ -92,14 +99,21 @@ class Optimizer:
             ]
             Fc = self.evaluate(xc)
 
+            print(f"Центр тяжести симплекса: {round(xc[0], 3)} {round(xc[1], 3)}"
+                  f"\nЗначение функции в центре: {round(Fc, 3)}")
+
             iteration += 1
+
+            print(f"Полученный базис\n{round(self.x[0][0], 3)} {round(self.x[0][1], 3)}\n"
+                  f"{round(self.x[1][0], 3)} {round(self.x[1][1], 3)}\n"
+                  f"{round(self.x[2][0], 3)} {round(self.x[2][1], 3)}\n")
 
             # проверка условия остановки
             sigma = sqrt(sum([(self.F[i] - Fc) ** 2 for i in range(self.n + 1)]) / (self.n + 1))
             if sigma < self.epsilon:
                 x_min = np.argmin(self.F)
                 print("Критерий остановки выполнен")
-                print(f"Минимум в точке: {self.x[x_min]}")
+                print(f"Минимум в точке: {self.x[x_min][0]:.6f} {self.x[x_min][1]:.6f}")
                 print(f"Значение функции: {self.F[x_min]:.6f}")
                 print(f"Итераций выполнено: {iteration}")
                 break
