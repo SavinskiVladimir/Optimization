b93b411f0956ba10e0c78cc08edd31e2f6cd33ca
Савинский Владимир <vovka_savinskiy@mail.ru>
2026-02-14 09:42:03 +0300

    Реализация алгоритма Нелдера-Мида

--- Nelder-Mid.py
+++ Nelder-Mid.py
@@ -1,10 +1,109 @@
-
+from math import sqrt
+import numpy as np
 
 class Optimizer:
 
     def __init__(self):
-        pass
+        self.n = 2
+        self.m = 0.5
+        self.gamma = 0.45
+        self.beta = 2.2
+        self.epsilon = 0.1
+        self.x = [[1.5, 0.2], [], []]
+        self.F = None
 
-    def evaluate(self, x, y):
-        return 7 * x ** 2 + 2 * x * y + 5 * y ** 2 + x - 10 * y
+    def evaluate(self, x):
+        return 7 * x[0] ** 2 + 2 * x[0] * x[1] + 5 * x[1] ** 2 + x[0] - 10 * x[1]
 
+    def optimize(self):
+        # расчёт двух вершин начального симплекса
+        delta1 = (sqrt(self.n + 1) - 1) / (self.n * sqrt(2)) * self.m
+        delta2 = (sqrt(self.n + 1) + self.n - 1) / (self.n * sqrt(2)) * self.m
+        self.x[1] = [self.x[0][0] + delta1, self.x[0][1] + delta2]
+        self.x[2] = [self.x[0][0] + delta2, self.x[0][1] + delta1]
+        self.F = [self.evaluate(self.x[i]) for i in range(self.n + 1)] # оценка вершин
+
+        iteration = 0
+        while True:
+            k1, k2, k = np.argsort(self.F) # сортировка вершин
+            # определение центра тяжести (без максимальной вершины)
+            xc = [
+                (self.x[k1][0] + self.x[k2][0]) / self.n,
+                (self.x[k1][1] + self.x[k2][1]) / self.n
+            ]
+            # отражение максимальной вершины
+            x = [
+                2 * xc[0] - self.x[k][0],
+                2 * xc[1] - self.x[k][1]
+            ]
+            F = self.evaluate(x)
+
+            squeeze_flag, reduction_flag = False, False
+
+            if F < self.F[k]:
+                self.x[k] = x
+                self.F[k] = F
+                if self.F[k] < self.F[k1]:
+                    x = [
+                        xc[0] + self.beta * (self.x[k][0] - xc[0]),
+                        xc[1] + self.beta * (self.x[k][1] - xc[1])
+                    ]
+                    F = self.evaluate(x)
+                    if F < self.F[k]:
+                        self.x[k] = x
+                        self.F[k] = F
+                    else:
+                        squeeze_flag = True
+                else:
+                    squeeze_flag = True
+            else:
+                squeeze_flag = True
+
+            # сжатие
+            if squeeze_flag:
+                if self.F[k1] < F < self.F[k]:
+                    x = [
+                        xc[0] + self.gamma * (self.x[k][0] - xc[0]),
+                        xc[1] + self.gamma * (self.x[k][1] - xc[1])
+                    ]
+                    F = self.evaluate(x)
+                    if F < self.F[k]:
+                        self.x[k] = x
+                        self.F[k] = F
+                    else:
+                        reduction_flag = True
+                else:
+                    reduction_flag = True
+
+            # редукция
+            if reduction_flag:
+                r = np.argmin(self.F)
+                xr = self.x[r]
+                for i in range(self.n + 1):
+                    if i != r:
+                        self.x[i] = [
+                            xr[0] + 0.5 * (self.x[i][0] - xr[0]),
+                            xr[1] + 0.5 * (self.x[i][1] - xr[1])
+                        ]
+            # центр тяжести всего симплекса
+            xc = [
+                (self.x[0][0] + self.x[1][0] + self.x[2][0]) / (self.n + 1),
+                (self.x[0][1] + self.x[1][1] + self.x[2][1]) / (self.n + 1)
+            ]
+            Fc = self.evaluate(xc)
+
+            iteration += 1
+
+            # проверка условия остановки
+            sigma = sqrt(sum([(self.F[i] - Fc) ** 2 for i in range(self.n + 1)]) / (self.n + 1))
+            if sigma < self.epsilon:
+                x_min = np.argmin(self.F)
+                print("Критерий остановки выполнен")
+                print(f"Минимум в точке: {self.x[x_min]}")
+                print(f"Значение функции: {self.F[x_min]:.6f}")
+                print(f"Итераций выполнено: {iteration}")
+                break
+
+
+opt = Optimizer()
+opt.optimize()
